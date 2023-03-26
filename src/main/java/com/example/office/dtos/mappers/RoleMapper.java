9accb66391b81d84a707f8f975598daa50d3bbc1
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2023-03-26 02:35:52 +0000

    fix MapStruct mappers

--- src/main/java/com/example/office/dtos/mappers/RoleMapper.java
+++ src/main/java/com/example/office/dtos/mappers/RoleMapper.java
@@ -2,10 +2,12 @@ package com.example.office.dtos.mappers;
 
 import com.example.office.domain.Role;
 import com.example.office.dtos.RoleDTO;
+import org.mapstruct.Mapper;
 
 /**
  * Mapper for the entity {@link Role} and its DTO {@link RoleDTO}
  */
 
+@Mapper(componentModel = "spring")
 public interface RoleMapper extends EntityMapper<Role, RoleDTO> {
 }
